f1c2bfb2feec94378806bca429f41c6f1e9948e6
Arthur Song <arth3rs0ng@gmail.com>
2019-04-16 20:30:27 +0000

    fake list_of_divisions method

--- lib/current_ufc_champions.rb
+++ lib/current_ufc_champions.rb
@@ -1,4 +1,5 @@
-require "current_ufc_champions/version"
+require_relative "./current_ufc_champions/version"
+require_relative "./current_ufc_champions/cli"
 
 module CurrentUfcChampions
   class Error < StandardError; end
--- lib/current_ufc_champions/cli.rb
+++ lib/current_ufc_champions/cli.rb
@@ -3,9 +3,24 @@
 class CurrentUfcChampions::CLI
   
   def call
-    puts "Current UFC Champions:"
-    
+    puts "Welcome to Current UFC Champions"
+    list_of_divisions
   end
   
+  def list_of_divisions
+    puts "List of Divisions:"
+    puts "1. Heavyweight (Up to 265 pounds)"
+    puts "2. Light heavyweight (205)"
+    puts "3. Middleweight (185)"
+    puts "4. Welterweight (170)"
+    puts "5. Lightweight (155)"
+    puts "6. Men's featherweight (145)"
+    puts "7. Men's bantamweight (135)"
+    puts "8. Men's flyweight (125)"
+    puts "9. Women's featherweight (145)"
+    puts "10. Women's bantamweight (135)"
+    puts "11. Women's flyweight (125)"
+    puts "12. Strawweight (115)"
+  end
   
 end
