9508e486ab030f9e72e09924d621e855882ae264
Arthur Song <arth3rs0ng@gmail.com>
2019-04-16 20:11:23 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in current_ufc_champions.gemspec
+gemspec
---
+++ lib/current_ufc_champions.rb
@@ -0,0 +1,6 @@
+require "current_ufc_champions/version"
+
+module CurrentUfcChampions
+  class Error < StandardError; end
+  # Your code goes here...
+end
